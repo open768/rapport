4160edefcfc4fe8feaf7dd82b7f55a9223080e3b
chicken Katsu <open768@yahoo.co.uk>
2016-06-10 10:50:47 +0100

    stylesheet and blank data

---
+++ inc/inc-secret.php
@@ -0,0 +1,20 @@
+<?php
+
+/**************************************************************************
+Copyright (C) Chicken Katsu 2016
+
+This code is protected by copyright under the terms of the 
+Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License
+http://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
+
+For licenses that allow for commercial use please contact cluck@chickenkatsu.co.uk
+
+// USE AT YOUR OWN RISK - NO GUARANTEES OR ANY FORM ARE EITHER EXPRESSED OR IMPLIED
+**************************************************************************/
+
+class cSecret{
+	const GOOGLE_TAG_ID = "not set";
+	const LICENSED_TO = "Not Licensed";
+	const LICENSE_COMMENT = "** This is a Freemium product **";
+}
+?>
