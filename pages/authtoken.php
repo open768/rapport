261cb988b115331ac33e1ac55c19760a011b4a86
open seven six ate <open768@yahoo.co.uk>
2018-05-27 21:15:56 +0100

    $home used

--- pages/authtoken.php
+++ pages/authtoken.php
@@ -19,7 +19,6 @@ require_once("$phpinc/ckinc/debug.php");
 require_once("$phpinc/ckinc/session.php");
 require_once("$phpinc/ckinc/common.php");
 require_once("$phpinc/ckinc/http.php");
-require_once("$root/../inc/secret.php");
 	
 cSession::set_folder();
 session_start();
@@ -38,9 +37,8 @@ cRender::html_header("AuthToken");
 cRender::force_login();
 
 cRender::show_top_banner("AuthToken"); 
-cAppDynCredentials::$encryption_key
 $sToken = cAppDynCredentials::get_login_token();
-$sURL = cHeader::get_server().dirname($_SERVER["SCRIPT_NAME"])."/index.php";
+$sURL = cHeader::get_server().dirname($_SERVER["SCRIPT_NAME"])."/$home/index.php";
 $sURL = cHttp::build_URL($sURL,cRender::LOGIN_TOKEN_QS, $sToken);
 ?>
 
