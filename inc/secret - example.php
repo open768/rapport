c89f43dc6da987d45edd7dd09e5cd7133fda1e4b
open seven six ate <open768@yahoo.co.uk>
2022-01-17 21:04:18 +0000

    added secret example

---
+++ inc/secret - example.php
@@ -0,0 +1,29 @@
+<?php
+/**************************************************************************
+Copyright (C) Chicken Katsu 2018
+
+This code is protected by copyright under the terms of the 
+Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License
+http://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
+
+For licenses that allow for commercial use please contact cluck@chickenkatsu.co.uk
+
+// USE AT YOUR OWN RISK - NO GUARANTEES OR ANY FORM ARE EITHER EXPRESSED OR IMPLIED
+**************************************************************************/
+require_once("$phpinc/ckinc/newrelic.php");
+
+class cSecret{
+	const GOOGLE_TAG_ID = "";
+	
+	const LICENSED_TO = "Not Licensed";
+	const LICENSE_COMMENT = "** contact cluck@chickenkatsu.co.uk for licenses";
+	const ENCRYPTION_KEY = "";
+	
+	const LINKEDIN_CLIENTID = "";
+	const LINKEDIN_SECRET = "";
+	
+	const ENABLE_NR_MONITORING = false;
+	static $NR_DEV = "";
+	static $NR_PROD = "";
+}
+?>
