9950201043b0694dcd1d9f7fee1f3da73b05d638
open seven six ate <open768@yahoo.co.uk>
2023-04-25 22:12:39 +0100

    bugfix and added cards

--- pages/tier/tierextcalls.php
+++ pages/tier/tierextcalls.php
@@ -31,58 +31,56 @@ $gsTierQs = cRenderQS::get_base_tier_QS($oTier);
 function render_tier_ext($poApp, $poTier, $poData){
 	global $LINK_SESS_KEY;
 	
-	if (sizeof($poData) > 0){
-	
+	if (count($poData) == 0){
+		cCommon::messagebox("no External Tiers Found");
+	}else{
 		$tierlink=cRender::getTierLink($poApp->name, $poApp->id, $poTier->name,  $poTier->id);
-		
-		?><table border=1 cellspacing=0>
-			<tr>
-				<th width=700><?=$tierlink?></th>
-				<th colspan=4>Calls per min</th>
-				<th rowspan=2 width=80>max response Times (ms)</th>
-			</tr>
-			<tr>
-				<th width=700>other tier</th>
-				<th width=80>max</th>
-				<th width=80>min</th>
-				<th width=80>avg</th>
-				<th width=80>total</th>
-			</tr><?php
 
-			$sBaseQs = cRenderQS::get_base_tier_QS($poTier);
-			foreach ( $poData as $oDetail){
-				cDebug::write("DEBUG: ".$oDetail->name);
-				$other_tier = $oDetail->name;
-				$oCalls = $oDetail->calls;
-				$oTimes = $oDetail->times;
-				
-				if ($oCalls && $oTimes && ($oTimes->max > 0)){
-						$sQs = cHttp::build_qs($sBaseQs, cRenderQS::FROM_TIER_QS, $poTier->name);
-						$sQs = cHttp::build_qs($sQs, cRenderQS::TO_TIER_QS, $other_tier);
-						?><tr>
-							<td><a href='tiertotier.php?<?=$sQs?>'><?=$other_tier?></a></td>
-							<td align="middle"><?=$oCalls->max?></td>
-							<td align="middle"><?=$oCalls->min?></td>
-							<td align="middle"><?=$oCalls->avg?></td>
-							<td align="middle"><?=$oCalls->sum?></td>
-							<td align="middle" bgcolor="lightgrey"><?=$oTimes->max?></td>
-						</tr><?php
+		cRenderCards::card_start("Details");
+			cRenderCards::body_start();
+			
+				?><table border=1 cellspacing=0>
+					<tr>
+						<th width=700><?=$tierlink?></th>
+						<th colspan=4>Calls per min</th>
+						<th rowspan=2 width=80>max response Times (ms)</th>
+					</tr>
+					<tr>
+						<th width=700>other tier</th>
+						<th width=80>max</th>
+						<th width=80>min</th>
+						<th width=80>avg</th>
+						<th width=80>total</th>
+					</tr><?php
+
+					$sBaseQs = cRenderQS::get_base_tier_QS($poTier);
+					foreach ( $poData as $oDetail){
+						cDebug::write("DEBUG: ".$oDetail->name);
+						$other_tier = $oDetail->name;
+						$oCalls = $oDetail->calls;
+						$oTimes = $oDetail->times;
+						
+						if ($oCalls && $oTimes && ($oTimes->max > 0)){
+								$sQs = cHttp::build_qs($sBaseQs, cRenderQS::FROM_TIER_QS, $poTier->name);
+								$sQs = cHttp::build_qs($sQs, cRenderQS::TO_TIER_QS, $other_tier);
+								?><tr>
+									<td><a href='tiertotier.php?<?=$sQs?>'><?=$other_tier?></a></td>
+									<td align="middle"><?=$oCalls->max?></td>
+									<td align="middle"><?=$oCalls->min?></td>
+									<td align="middle"><?=$oCalls->avg?></td>
+									<td align="middle"><?=$oCalls->sum?></td>
+									<td align="middle" bgcolor="lightgrey"><?=$oTimes->max?></td>
+								</tr><?php
+						}
 				}
-		}
-		?></table><?php
+				?></table><?php
+			cRenderCards::body_end();
+		cRenderCards::card_end();
 	}
 }
 //####################################################################
 cRenderHtml::header("External tier calls");
 cRender::force_login();
-
-$oCred = cRenderObjs::get_AD_credentials();
-if ($oCred->restricted_login == null){
-	cRenderMenus::show_app_functions();
-	cRenderMenus::show_tier_functions();
-	cRenderMenus::show_tier_menu("Change Tier to", cCommon::filename());
-}
-
 
 //********************************************************************
 if (cAD::is_demo()){
@@ -90,14 +88,23 @@ if (cAD::is_demo()){
 	cRenderHtml::footer();
 	exit;
 }
+
+$oCred = cRenderObjs::get_AD_credentials();
+
 //********************************************************************
-//********************************************************************
-//####################################################################
-cCommon::flushprint ("<br>");
+cRenderCards::card_start("External calls in $oTier->name");
+	cRenderCards::action_start();
+		cRender::button("show as graphs", "tierextgraph.php?$gsTierQs");
+		if ($oCred->restricted_login == null){
+			cRenderMenus::show_app_functions();
+			cRenderMenus::show_tier_functions();
+			cRenderMenus::show_tier_menu("Change Tier to", cCommon::filename());
+		}
+	cRenderCards::action_end();
+cRenderCards::card_end();
+
 $oTimes = cRender::get_times();
 $oResponse =$oTier->GET_ext_details($oTimes);
-cRender::button("show as graphs", "tierextgraph.php?$gsTierQs");
-?><h2>External calls in <?=$oTier->name?>)</h2><?php
 render_tier_ext($oApp, $oTier, $oResponse);
 
 cRenderHtml::footer();
